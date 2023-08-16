1c2c5c3fe7053924fdde6547928c2b51e0d3388d
OMEN\Dominik <dominik@adnett.de>
2023-08-16 20:54:47 +0200

    .

--- Ntfy/module.php
+++ Ntfy/module.php
@@ -30,10 +30,36 @@ declare(strict_types=1);
 
 		public function GetConfigurationForm()
 		{
-			$use_auth = $this->ReadPropertyBoolean('USE_AUTH') ?? false;
-			$use_token = $this->ReadPropertyBoolean('USE_TOKEN') ?? false;
+			$use_auth = $this->ReadPropertyBoolean('USE_AUTH');
+			$use_token = $this->ReadPropertyBoolean('USE_TOKEN');
 
-			return '{ "elements": [ { "type": "ValidationTextBox", "name": "URL", "caption": "Server URL (required)" }, { "type": "CheckBox", "name": "USE_AUTH", "caption": "Use authentication", "onChange": "NTFY_UseAuthentication($id, $USE_AUTH);" }, { "type": "ExpansionPanel", "name": "AUTH_PANEL", "caption": "Authentication", "visible": '.$use_auth.', "items":[ { "type": "CheckBox", "name": "USE_TOKEN", "caption": "Use Token instead of credentials", "onChange": "NTFY_ToggleUseToken($id, $USE_TOKEN);" }, { "type": "PasswordTextBox", "name": "TOKEN", "caption": "Application Token (required)", "visible": '.$use_token.' }, { "type": "ValidationTextBox", "name": "USERNAME", "caption": "Username (required)", "visible": '.!$use_token.' }, { "type": "PasswordTextBox", "name": "PASSWORD", "caption": "Password (required)", "visible": '.!$use_token.' } ] } ], "actions": [ { "type": "ValidationTextBox", "name": "TOPIC", "caption": "Topic" }, { "type": "Button", "caption": "Send test message",  "onClick": "if (NTFY_SendTestMessage($id, $TOPIC)) echo \'OK\'; else echo \'Error\';" } ], "status": [ { "code": 102, "icon": "active", "caption": "OK" }, { "code": 201, "icon": "error", "caption": "An error occurred - please check the log" }, { "code": 202, "icon": "error", "caption": "Invalid URL" }, { "code": 203, "icon": "error", "caption": "Unauthorized" }, { "code": 204, "icon": "error", "caption": "Forbidden" } ] }';
+			var_dump($use_auth);
+
+			return 
+			'{
+				"elements": [
+					{ "type": "ValidationTextBox", "name": "URL", "caption": "Server URL (required)" },
+					{ "type": "CheckBox", "name": "USE_AUTH", "caption": "Use authentication", "onChange": "NTFY_UseAuthentication($id, $USE_AUTH);" },
+					{ "type": "ExpansionPanel", "name": "AUTH_PANEL", "caption": "Authentication", "visible": '.$use_auth.', "items":[
+							{ "type": "CheckBox", "name": "USE_TOKEN", "caption": "Use Token instead of credentials", "onChange": "NTFY_ToggleUseToken($id, $USE_TOKEN);" },
+							{ "type": "PasswordTextBox", "name": "TOKEN", "caption": "Application Token (required)", "visible": '.$use_token.' },
+							{ "type": "ValidationTextBox", "name": "USERNAME", "caption": "Username (required)", "visible": '.!$use_token.' },
+							{ "type": "PasswordTextBox", "name": "PASSWORD", "caption": "Password (required)", "visible": '.!$use_token.' }
+						]
+					}
+				],
+				"actions": [
+					{ "type": "ValidationTextBox", "name": "TOPIC", "caption": "Topic" },
+					{ "type": "Button", "caption": "Send test message",  "onClick": "if (NTFY_SendTestMessage($id, $TOPIC)) echo \'OK\'; else echo \'Error\';" }
+				],
+				"status": [
+					{ "code": 102, "icon": "active", "caption": "OK" },
+					{ "code": 201, "icon": "error", "caption": "An error occurred - please check the log" },
+					{ "code": 202, "icon": "error", "caption": "Invalid URL" },
+					{ "code": 203, "icon": "error", "caption": "Unauthorized" },
+					{ "code": 204, "icon": "error", "caption": "Forbidden" }
+				]
+			}';
 		}
 
 		public function UseAuthentication(bool $status)
